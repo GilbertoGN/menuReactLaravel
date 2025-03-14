3c1633dd83a7375969c26759d3a1e5cc1252162c
JPachecoOnceTV <jpacheco@canalonce.ipn.mx>
2025-03-14 17:06:32 -0600

    fix: Se quita ruta en blanco, se define ruta inicial home

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    return redirect('/home');
+});
+
+Route::get('/home', function () {
     return Inertia:: render (('Home'));
 });
