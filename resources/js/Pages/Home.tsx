188735568d84e7035fdf5c1ac624e25021983289
JPachecoOnceTV <jpacheco@canalonce.ipn.mx>
2025-03-14 17:56:08 -0600

    feat: animación iconos

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,5 +1,3 @@
-
-
 import React from 'react';
 import SideBar from '@/components/SideBar';
 import Img1 from "../../assets/img/SCO__Administracion.png"
@@ -17,18 +15,22 @@ const Home: React.FC<HomeProps> = ({ title }) => {
             <SideBar />
             <div className="contenedor-inicial">
                 <div className="container text-center" >
-                    <div className="row align-items-start">
-                        <div className="col fade-in">
+                    <div className="row justify-content-between">
+                        <div className="col-3 fade-in">
                             <img src={Img1} alt="Logo"></img>
+                            <p className="text-3xl text-center">Comunicación</p>
                         </div>
-                        <div className="col fade-in">
-                            <img src={Img2} alt="Logo"></img>
-                        </div>
-                        <div className="col fade-in">
+                        <div className="col-3 fade-in">
                             <img src={Img3} alt="Logo"></img>
+                            <p className="text-3xl text-center">Comunicación</p>
                         </div>
-                        <div className="col fade-in">
+                        <div className="col-3 fade-in">
+                            <img src={Img2} alt="Logo"></img>
+                            <p className="text-3xl text-center">Comunicación</p>
+                        </div>
+                        <div className="col-3 fade-in">
                             <img src={Img4} alt="Logo"></img>
+                            <p className="text-3xl text-center">Comunicación</p>
                         </div>
                     </div>
                 </div>
